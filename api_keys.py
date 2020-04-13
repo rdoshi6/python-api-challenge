2a017181086947fc5bb29e9405f5a2b6aa78e734
Rohan Doshi <RohanDoshi@Rohans-MacBook-Pro.local>
2020-04-12 19:14:55 -0500

    first commit

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "6d4b1b9316ba9df070e0338eb65024da"
+
+# Google API Key
+g_key = "AIzaSyCQQxTPwERJ0K3g6eY9BDV89ClBRk5TlKA"
+#g_key = "AIzaSyB1jqoeWOBQnvTPKeFF6dYktu3gPJ9nImk"
